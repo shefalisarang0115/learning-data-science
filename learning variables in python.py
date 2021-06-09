3f36104e7bab4c24f7ed8279c1eb0354b7b6cf0a
shefalisarang0115 <64216448+shefalisarang0115@users.noreply.github.com>
2021-06-09 17:15:57 +0530

    Add files via upload

---
+++ learning variables in python.py
@@ -0,0 +1,26 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 2+3
+5
+>>> 
+>>> 10*2
+20
+>>> print("welcome")
+welcome
+>>> rent=1220
+>>> gas=202.5
+>>> groceries= 305.6
+>>> print(rent)
+1220
+>>> total=rent+gas+groceries
+>>> print(total)
+1728.1
+>>> rent=1400
+>>> item1="rent"
+>>> item2="gas"
+>>> item3="groceries"
+>>> print("Expense items:",item1,item2,item3)
+Expense items: rent gas groceries
+>>> True = 4
+SyntaxError: cannot assign to True
+>>> 
